05c581aacf911b94a8e60dcf494b2b7a1bc845fa
Kyle Giard-Chase <kyle.giard-chase@metaltoad.com>
2015-06-20 13:53:11 -0700

    Add secondary pages.

--- webroot/services/StepsFactory.js
+++ webroot/services/StepsFactory.js
@@ -1,7 +1,7 @@
 mtmIntern.factory('StepsFactory', function StepsFactory() {
   var factory = {};
   factory.steps = [
-    { name: "Welcome", complete: false, link: "welcome", id: 1 },
+    { name: "Welcome", complete: false, othersteps: { one: 'one', two: 'two'}, link: "welcome", id: 1 },
     { name: "Getting Started", complete: false, link: "getting-started", id: 2 },
     { name: "Setting up Your Dev Environment", complete: false, link: "setting-up-your-dev-environment", id: 3 },
     { name: "Quality Assurance Basics", complete: false, link: "qa-basics", id: 4 },
