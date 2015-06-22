2ee929c0fa72c241ed146aedc8e323edecc992ab
Kyle Giard-Chase <kyle.giard-chase@metaltoad.com>
2015-06-22 12:35:39 -0700

    Add modal states.

--- webroot/app.js
+++ webroot/app.js
@@ -11,9 +11,9 @@ mtmIntern.config(function($stateProvider) {
     templateUrl: 'partials/getting-started.html'
   });
 
-  $stateProvider.state('setting-up-your-dev-environment', {
-    url: '/setting-up-your-dev-environment',
-    templateUrl: 'partials/setting-up-your-dev-environment.html'
+  $stateProvider.state('setting-up', {
+    url: '/setting-up',
+    templateUrl: 'partials/setting-up.html'
   });
 
   $stateProvider.state('qa-basics', {
@@ -24,6 +24,146 @@ mtmIntern.config(function($stateProvider) {
   $stateProvider.state('FAQ', {
     url: '/FAQ',
     templateUrl: 'partials/FAQ.html'
+  });
+
+  //begin modal views
+  //adapted from blog post: http://www.sitepoint.com/creating-stateful-modals-angularjs-angular-ui-router/
+
+  //base view - must be nested inside each section
+  $stateProvider.state("qa-basics.modal", {
+    views:{
+      "qa-basics.modal": {
+        templateUrl: "partials/modal.html"
+      }
+    },
+    //escape on esc keypress or upon click of modal backdrop
+    onEnter: ["$state", function($state) {
+      $(document).on("keyup", function(e) {
+        if(e.keyCode == 27) {
+          $(document).off("keyup");
+          $state.go("qa-basics");
+        }
+      });
+
+      $(document).on("click", ".Modal-backdrop, .Modal-holder", function() {
+        $state.go("qa-basics");
+      });
+
+      $(document).on("click", ".Modal-box, .Modal-box *", function(e) {
+        e.stopPropagation();
+      });
+    }],
+    abstract: true //can't be directly transitioned to
+  });
+
+  //Each step state
+
+  $stateProvider.state("qa-basics.modal.one", {
+    views:{
+      "qa-basics.modal": {
+        templateUrl: "partials/qa-basics.one.html"
+      }
+    }
+  });
+
+  $stateProvider.state("qa-basics.modal.two", {
+    views:{
+      "qa-basics.modal": {
+        templateUrl: "partials/qa-basics.two.html"
+      }
+    }
+  });
+
+  $stateProvider.state("qa-basics.modal.three", {
+    views:{
+      "qa-basics.modal": {
+        templateUrl: "partials/qa-basics.three.html"
+      }
+    }
+  });
+
+  $stateProvider.state("qa-basics.modal.four", {
+    views:{
+      "qa-basics.modal": {
+        templateUrl: "partials/qa-basics.four.html"
+      }
+    }
+  });
+
+  $stateProvider.state("qa-basics.modal.five", {
+    views:{
+      "qa-basics.modal": {
+        templateUrl: "partials/qa-basics.five.html"
+      }
+    }
+  });
+
+  $stateProvider.state("setting-up.modal", {
+    views:{
+      "setting-up.modal": {
+        templateUrl: "partials/modal.html"
+      }
+    },
+    //escape on esc keypress or upon click of modal backdrop
+    onEnter: ["$state", function($state) {
+      $(document).on("keyup", function(e) {
+        if(e.keyCode == 27) {
+          $(document).off("keyup");
+          $state.go("setting-up");
+        }
+      });
+
+      $(document).on("click", ".Modal-backdrop, .Modal-holder", function() {
+        $state.go("setting-up");
+      });
+
+      $(document).on("click", ".Modal-box, .Modal-box *", function(e) {
+        e.stopPropagation();
+      });
+    }],
+    abstract: true //can't be directly transitioned to
+  });
+
+  //Each step state
+
+  $stateProvider.state("setting-up.modal.one", {
+    views:{
+      "setting-up.modal": {
+        templateUrl: "partials/setting-up.one.html"
+      }
+    }
+  });
+
+  $stateProvider.state("setting-up.modal.two", {
+    views:{
+      "setting-up.modal": {
+        templateUrl: "partials/setting-up.two.html"
+      }
+    }
+  });
+
+  $stateProvider.state("setting-up.modal.three", {
+    views:{
+      "setting-up.modal": {
+        templateUrl: "partials/setting-up.three.html"
+      }
+    }
+  });
+
+  $stateProvider.state("setting-up.modal.four", {
+    views:{
+      "setting-up.modal": {
+        templateUrl: "partials/setting-up.four.html"
+      }
+    }
+  });
+
+  $stateProvider.state("setting-up.modal.five", {
+    views:{
+      "setting-up.modal": {
+        templateUrl: "partials/setting-up.five.html"
+      }
+    }
   });
 
 });
--- webroot/services/StepsFactory.js
+++ webroot/services/StepsFactory.js
@@ -1,17 +1,12 @@
 mtmIntern.factory('StepsFactory', function StepsFactory() {
   var factory = {};
   factory.steps = [
-    { name: "Welcome", complete: false, othersteps: { one: 'one', two: 'two'}, link: "welcome", id: 1 },
-    { name: "Getting Started", complete: false, link: "getting-started", id: 2 },
-    { name: "Setting up Your Dev Environment", complete: false, link: "setting-up-your-dev-environment", id: 3 },
-    { name: "Quality Assurance Basics", complete: false, link: "qa-basics", id: 4 },
-    { name: "FAQ", complete: false, link: "FAQ", id: 5 },
+    { name: "Welcome", w_complete: false },
+    { name: "Getting Started", gs_complete: false },
+    { name: "Setting up Your Dev Environment", setting_complete: false },
+    { name: "Quality Assurance Basics", qa_complete: false },
+    { name: "FAQ", faq_complete: false },
   ];
-
-  factory.markComplete = function($sectionID) {
-
-  }
-
 
   return factory;
 });
